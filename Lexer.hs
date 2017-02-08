fd8dd48b19b0df17f50b3496d6fd5cd669aa07ce
Brian Steiner <briansteiner95@gmail.com>
2017-02-07 19:22:19 -0500

    moved the Lexer-like functions to the lexer. need to reconstruct them entirely to put in the concept of states

--- Lexer.hs
+++ Lexer.hs
@@ -13,3 +13,20 @@ tokens =
 		")"
 	]
 
+tokenize :: [Char] -> [TOKEN]
+tokenize "" = []
+tokenize x = tokenizeHelper x "" []
+
+tokenizeHelper :: [Char] -> [Char] -> [TOKEN] -> [TOKEN]
+tokenizeHelper "" _ tokens = tokens
+tokenizeHelper (fst:inpt) buff tkns = 
+	if (elem [fst] tokens) then tokenizeHelper inpt buff tkns++[(makeTOKEN [fst])]
+	else error "ERROR"
+
+
+
+
+makeTOKEN :: [Char] -> TOKEN
+makeTOKEN "(" = T_LParen
+makeTOKEN ")" = T_RParen
+makeTOKEN x = Invalid
--- main.hs
+++ main.hs
@@ -13,22 +13,3 @@ main = do
 
 message :: IO ()
 message = do putStrLn "Lexing..."
-
-
-tokenize :: [Char] -> [TOKEN]
-tokenize "" = []
-tokenize x = tokenizeHelper x "" []
-
-tokenizeHelper :: [Char] -> [Char] -> [TOKEN] -> [TOKEN]
-tokenizeHelper "" _ tokens = tokens
-tokenizeHelper (fst:inpt) buff tkns = 
-	if (elem [fst] tokens) then tokenizeHelper inpt buff tkns++[(makeTOKEN [fst])]
-	else error "ERROR"
-
-
-
-
-makeTOKEN :: [Char] -> TOKEN
-makeTOKEN "(" = T_LParen
-makeTOKEN ")" = T_RParen
-makeTOKEN x = Invalid

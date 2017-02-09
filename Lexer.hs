7b7b6c9d89f33d692019e7fae1e82987325ebcf3
Brian Steiner <briansteiner95@gmail.com>
2017-02-09 01:41:20 -0500

    It works!!!

--- Lexer.hs
+++ Lexer.hs
@@ -3,13 +3,13 @@ module Lexer where
 
 
 data Token = T_name String
-		   | T_LParen
-		   | T_RParen
-		   | T_if
-		   | T_in
-		   | T_end
-		   | Invalid
-		   		deriving (Eq, Show)
+           | T_LParen
+           | T_RParen
+           | T_if
+           | T_in
+           | T_end
+           | Invalid
+                deriving (Eq, Show)
 
 
 data State = State String String [Token] Int deriving (Show)
@@ -18,13 +18,13 @@ data State = State String String [Token] Int deriving (Show)
 
 symbolTable :: [(Int, String)]
 symbolTable =
-	[
-		(1, "("),
-		(2, ")"),
-		(3, "i"),
-		(5, "n"),
-		(4, "f")
-	]
+    [
+        (1, "("),
+        (2, ")"),
+        (3, "i"),
+        (5, "n"),
+        (4, "f")
+    ]
 getKey :: (Int, String) -> Int
 getKey (k, p) = k
 getPair :: (Int, String) -> String
@@ -52,18 +52,17 @@ tokenizeHelp :: State -> [Token]
 tokenizeHelp s@(State [] b t n) = t
 --one char input
 tokenizeHelp s@(State (fst:[]) b t n) = tokenizeHelp (processState (State [] (b++[fst]) t n))
---two char input??
 --multiple char input
---tokenizeHelp s@(State (fst:i) b t n) = tokenizeHelp ( )
+tokenizeHelp s@(State (fst:i) b t n) = tokenizeHelp (processState (State i (b++[fst]) t n))
+
 
 --takes a state, add input to buffer, read buffer
 --needs more conditions!!!
 processState :: State -> State
-processState s@(State i b t 0) =
-	if((makeToken s) /= Invalid)
-		then (State i "" (t++[(makeToken s)]) (makePath (makeToken s) s))
-	else
-		error "glambis"
+processState s@(State i b t n) =
+    if((makeToken s) /= Invalid)
+        then (State i "" (t++[(makeToken s)]) (makePath (makeToken s) s))
+    else s
 --kill condition?
 
 --(stateCons (State connections) logic to be implemented)
@@ -81,11 +80,11 @@ makePath tkn s@(State _ b _ n) = head (filter (==(getNum (last ((makeTerminal tk
 
 makeToken :: State -> Token
 makeToken s@(State _ b _ n) =
-	if      (b=="(")  then T_LParen 
-	else if (b==")")  then T_RParen
-	else if (b=="if") then T_if
-	else if (b=="in") then T_in
-	else Invalid
+    if      (b=="(")  then T_LParen 
+    else if (b==")")  then T_RParen
+    else if (b=="if") then T_if
+    else if (b=="in") then T_in
+    else Invalid
 
 --makeToken "k" = T_kill
 
@@ -106,12 +105,12 @@ getAdjacentCons s@(State _ _ _ n) = filter (>0) (stateCons!!n)
 
 stateCons :: [[Int]]
 stateCons =
-	--	  (   )	  i   n   f
-	[
-		[ 1,  2,  3, -9, -9],
-		[-9, -9, -9, -9, -9], --1: ( found
-		[-9, -9, -9, -9, -9], --2: ) found
-		[-9, -9, -9,  5,  4], --3: proceed to 4 or 5
-		[-9, -9, -9, -9, -9], --4: in found
-		[-9, -9, -9, -9, -9]  --5: if found
-	]
+    --    (   )   i   n   f
+    [
+        [ 1,  2,  3, -9, -9],
+        [-9, -9, -9, -9, -9], --1: ( found
+        [-9, -9, -9, -9, -9], --2: ) found
+        [-9, -9, -9,  5,  4], --3: proceed to 4 or 5
+        [-9, -9, -9, -9, -9], --4: in found
+        [-9, -9, -9, -9, -9]  --5: if found
+    ]

f9ee6109095cf8da80599d4e40c3cea8e7905f4a
Brian Steiner <briansteiner95@gmail.com>
2017-05-24 18:14:20 -0400

    commented out grammar.

--- Grammar.hs
+++ Grammar.hs
@@ -5,13 +5,18 @@ import LanguageData
 import Lexer
 
 
-data Program = Program Block Token
+--data Program = Program Block Token
 
-data Block = Block Token StatementList Token
+--data Block = Block Token StatementList Token
 
 --WILLls BE ADDED TO; just need to have something compiling for now
-data StatementList = Statement
+--data StatementList = Statement
 
 --IS NOT INT
-data Statement = Int
+--data Statement = Int
+
+
+
+
+
 

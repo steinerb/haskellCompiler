62ce5948503269779dc5359f3de0ee5dfac89bc5
Brian Steiner <briansteiner95@gmail.com>
2017-06-06 21:26:38 -0400

    Created while statements as long as it's the last one in the program! will add multiple STMT while loops next!

--- AST.hs
+++ AST.hs
@@ -51,8 +51,17 @@ makeAST p@(Program b@(Block ((n@(STMTlistNode stmt)):stmtLst))) ts =
 
 --Initial Input in State is a STMT, BLOCK #0 disected in makeAST
 astLoop :: State -> Tree String
+--      state@(State i ts sl tr)
+
 --BASE CASE: If no tokens are left, return the tree
-astLoop state@(State _ [] _ tr) = tr
+astLoop state@(State _ [] _  tr) = tr
+
+--BLOCK
+--astLoop state@(State 
+--                    (i@(Ib (b@(Block ((n@(STMTlistNode stmt)):stmtLst) ))))
+--                    ts
+--                    (s:sl)
+--              )
 
 --STMT
 --VarDecl
@@ -62,6 +71,7 @@ astLoop state@(State (i@(Is (stmt@(VarDeclSTMT t id)))) ts [] tr) = astLoop
 --statements to go
 astLoop state@(State (i@(Is (stmt@(VarDeclSTMT t id)))) ts ((n@(STMTlistNode s)):sl) tr) = astLoop 
     ( State (Is s) (drop 2 ts) sl ( tr `makeChild` (Node "<Variable Declaration>" [(Node (show t) []), (Node (show id) [])]) ) )
+
 --AssignStatement
 --last statement
 astLoop state@(State (i@(Is (stmt@(AssignSTMT id expr)))) ts [] tr) = 
@@ -69,15 +79,15 @@ astLoop state@(State (i@(Is (stmt@(AssignSTMT id expr)))) ts [] tr) =
     if ( ((validIdToken (ts!!2)) == True ) ) 
         then astLoop ( State (EMPTY) (drop 3 ts) [] 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (drop 1 $ init $ show (ts!!2)) [])])) )
-    --if id equal to Boolean Expr M
+    --if id equal to Boolean Expr Multiple
     else if ((ts!!2) == T_LParen)
         then astLoop ( State (EMPTY) (dropUntilP (drop 2 ts)) []
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (takeUntilP (drop 2 ts))) [])])) )
-    --if id equal to int literal M (must go BEFORE int literal S)
+    --if id equal to int literal Multiple (must go BEFORE int literal Single)
     else if ((validIntSToken (ts!!2)) && (length ts >= 4) && ((ts!!3) == T_intOp))
         then astLoop ( State (EMPTY) (dropWhile (validIntM) (drop 2 ts)) [] 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (takeWhile (validIntM) (drop 2 ts))) [])])) )
-    --if id equal to BoolValS, string literal, or int literal S 
+    --if id equal to BoolValSingle, string literal, or int literal Single
     else if ( ((ts!!2) == T_true) || ((ts!!2) == T_false) || (validStrLitToken (ts!!2) == True) || (validIntSToken (ts!!2)))
         then astLoop ( State (EMPTY) (drop 3 ts) [] 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (ts!!2)) [])])) )
@@ -89,20 +99,21 @@ astLoop state@(State (i@(Is (stmt@(AssignSTMT id expr)))) ts ((n@(STMTlistNode s
     if ( ((validIdToken (ts!!2)) == True ) ) 
         then astLoop ( State (Is s) (drop 3 ts) sl 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (drop 1 $ init $ show (ts!!2)) [])])) )
-    --if id equal to Boolean Expr M
+    --if id equal to Boolean Expr Multiple
     else if ((ts!!2) == T_LParen)
         then astLoop ( State (Is s) (dropUntilP (drop 2 ts)) sl
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (takeUntilP (drop 2 ts))) [])])) )
-    --if id equal to int literal M (must go BEFORE int literal S)
+    --if id equal to int literal Multiple (must go BEFORE int literal Single)
     else if ((validIntSToken (ts!!2)) && (length ts >= 4) && ((ts!!3) == T_intOp))
         then astLoop ( State (Is s) (dropWhile (validIntM) (drop 2 ts)) sl 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (takeWhile (validIntM) (drop 2 ts))) [])])) )
-    --if id equal to BoolValS, string literal, or int literal S 
+    --if id equal to BoolValSingle, string literal, or int literal Single 
     else if ( ((ts!!2) == T_true) || ((ts!!2) == T_false) || (validStrLitToken (ts!!2) == True) || (validIntSToken (ts!!2)))
         then astLoop ( State (Is s) (drop 3 ts) sl 
                         (tr `makeChild` (Node "<Assign Statement>" [(Node (show id) []), (Node (show (ts!!2)) [])])) )
     --error
     else error "AssignSTMT falsely identified in astLoop!!!"
+
 --PrintStatement
 --last statement
 astLoop state@(State (i@(Is (stmt@(PrintSTMT expr)))) ts [] tr) = astLoop 
@@ -112,6 +123,30 @@ astLoop state@(State (i@(Is (stmt@(PrintSTMT expr)))) ts [] tr) = astLoop
 astLoop state@(State (i@(Is (stmt@(PrintSTMT expr)))) ts ((n@(STMTlistNode s)):sl) tr) = astLoop 
     (State (Is s) (dropUntilP (drop 1 ts)) sl 
                         (tr `makeChild` (Node "<Print Statement>" [(Node (show $ drop 1 $ init $ (takeUntilP (drop 1 ts))) [])])) )
+
+--WhileStatement
+--last statement
+astLoop state@(State (i@(Is (stmt@(WhileSTMT boolExpr b@(Block ((subN@(STMTlistNode subStmt)):subStmtLst)))))) ts [] tr) = astLoop 
+    (State 
+        (EMPTY) 
+        (dropUntilB $ dropUntilP (drop 1 ts)) 
+        [] 
+        (tr `makeChild` (astLoop 
+                            (State 
+                                (Is subStmt) 
+                                ((drop 1) $ init $ (takeUntilB $ (dropUntilP (drop 1 ts)))) 
+                                (subStmtLst) 
+                                (Node "<BLOCK>" [])
+                            )
+                        )
+        ) 
+    )
+--statements to go
+
+
+
+
+
 
 
 

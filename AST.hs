f6843a928efddad6324c5232b843c298e2f48b59
Brian Steiner <briansteiner95@gmail.com>
2017-06-13 13:57:47 -0400

    Failed to properly typecheck a boolean expression. don't know if i'll be able to figure that out. moving on for now

--- AST.hs
+++ AST.hs
@@ -479,29 +479,81 @@ getScope :: NAME -> SymbolTable -> Int
 getScope n st = pullCurrentScope (getRow n st)
 
 
-
+--if ((tcbmHelp st tr) == "NOTSAME") then False
+--                        else True
 --Type Checking Functions
+passTypeCheckBoolM :: SymbolTable -> Tree String -> Bool
+passTypeCheckBoolM st tr =
+    if( (tcbmHelp st tr) /= "NOTSAME") then True 
+    else False
+
+tcbmHelp :: SymbolTable -> Tree String -> String
+tcbmHelp st tr@(Node n@("<Boolean Expression>") kids) = 
+    compareAndReturnType st 
+        (tcbmHelp st (kids!!0)) 
+        (tcbmHelp st (kids!!2))
+tcbmHelp st tr@(Node n kids) = decideType st (getVal tr)
+
+--tcbmHelp :: SymbolTable -> Tree String -> String
+--tcbmHelp st tr@(Node n@("<Boolean Expression>") kids) = 
+--    compareAndReturnType st 
+--        (tcbmHelp st (kids!!0)) 
+--        (tcbmHelp st (kids!!2))
+--tcbmHelp st tr@(Node n kids) = decideType st n
+
+
+--tcbmHelp :: SymbolTable -> Tree String -> String
+--tcbmHelp st tr@(Node n kids)  = 
+--    if ( ("Boolean Expression" `isInfixOf` (getVal (kids!!0))) && ("Boolean Expression" `isInfixOf` (getVal (kids!!2))) )
+--        then compareAndReturnType st
+--                (decideType st (tcbmHelp st (kids!!0)))
+--                (decideType st (tcbmHelp st (kids!!2))) 
+--    else if ("Boolean Expression" `isInfixOf` (getVal (kids!!2)))
+--        then compareAndReturnType st
+--                (decideType st (getVal (kids!!0)))
+--                (decideType st (tcbmHelp st (kids!!2)))
+--    else if ("Boolean Expression" `isInfixOf` (getVal (kids!!0)))
+--        then compareAndReturnType st
+--                (decideType st (tcbmHelp st (kids!!0)))
+--                (decideType st (getVal (kids!!2)))
+--    else
+--             compareAndReturnType st
+--                (decideType st (getVal (kids!!0)))
+--                (decideType st (getVal (kids!!2)))     
+
+
+compareAndReturnType :: SymbolTable -> String -> String -> String
+compareAndReturnType st a b = 
+    if      (a == "NOTSAME") then "NOTSAME"
+    else if (b == "NOTSAME") then "NOTSAME"
+    else if ((compareType st a b) == True) then decideType st a
+    else "NOTSAME"
+
 compareType :: SymbolTable -> String -> String -> Bool
 compareType st a b = 
-    if ((decideType st a) == (decideType st b)) then True
+    if      (a == "NOTSAME") then False
+    else if (b == "NOTSAME") then False
+    else if ((decideType st a) == (decideType st b)) then True
     else False
+
 
 decideType :: SymbolTable -> String -> String
 decideType st a = 
-    if ( a `isElem` st ) then (getType a st)
+    if (a == "NOTSAME") then "NOTSAME"
+    else if ( a `isElem` st ) then (getType a st)
     else if ((decideInt a st) == True) then "int"
     else if ((decideString a st) == True) then "string"
     else "boolean"
 --CONDITION: MUST RUN BEFORE decideString TO PICK UP NUMBERS BEFORE LENGTH IS COUNTED!!!
 decideInt :: String -> SymbolTable -> Bool
 decideInt a st = 
-    if ((True `notElem` (map (=='(') a)) && (("0" `isInfixOf` a) || ("1" `isInfixOf` a) || ("2" `isInfixOf` a) || ("3" `isInfixOf` a) || ("4" `isInfixOf` a) || ("5" `isInfixOf` a) || ("6" `isInfixOf` a) || ("7" `isInfixOf` a) || ("8" `isInfixOf` a) || ("9" `isInfixOf` a)) )
+    if ((True `notElem` (map (=='=') a)) && (("0" `isInfixOf` a) || ("1" `isInfixOf` a) || ("2" `isInfixOf` a) || ("3" `isInfixOf` a) || ("4" `isInfixOf` a) || ("5" `isInfixOf` a) || ("6" `isInfixOf` a) || ("7" `isInfixOf` a) || ("8" `isInfixOf` a) || ("9" `isInfixOf` a)) )
         then True 
     else False
 
 decideString :: String -> SymbolTable -> Bool
 decideString a st = 
-    if ( ((length a) > 1) && (a /= "true") && (a /= "false") && (True `notElem` (map (=='(') a)) && (True `notElem` (map (=='<') a)) ) 
+    if ( ((length a) > 1) && (a /= "true") && (a /= "false") && (a /= "NOTSAME") && (True `notElem` (map (=='=') a)) && (True `notElem` (map (=='<') a)) ) 
         then True
     else False
 
@@ -530,7 +582,7 @@ processKids (kid@(Node "<Assign Statement>" subKids):kids) curScope hiScope scop
     --SCOPECHECK ERROR FOR LHS
     if ( not (passScopeCheck curScope scopeMap table (getVal (subKids!!0))) ) 
         then error "ERROR: SCOPECHECK FAILED: [this error won't ever be reached. this is a passScopeCheck example.]"
-    --SCOPECHECK ERROR FOR RHS IF ID
+    --SCOPECHECK ERROR FOR RHS: IF ID
     else if ( (isValidId (getVal (subKids!!1))) && 
                 ( 
                     ((getVal (subKids!!1)) `isNotElem` table) || 
@@ -538,16 +590,19 @@ processKids (kid@(Node "<Assign Statement>" subKids):kids) curScope hiScope scop
                 ) 
             )
         then error ("ERROR: SCOPECHECK FAILED: "++(getVal (subKids!!1))++" is not in scope!!!")
-    --SCOPECHECK ERROR FOR RHS IF CONTAINS AN ID (BoolExpr Multiple and possibly others)
+    --SCOPECHECK ERROR FOR RHS: IF CONTAINS AN ID (BoolExpr Multiple and possibly others)
     else if ( 
                 (containsValidId (getVal (subKids!!1))) && 
                 ( False `elem` (map (passScopeCheck curScope scopeMap table) (retrieveValidIds (getVal (subKids!!1)))) ) 
             ) 
         then error "ERROR: SCOPECHECK FAILED: [this error won't ever be reached, passScopeCheck used instead]"
-    --TYPECHECK ERROR FOR BOTH LHS&RHS
+    --TYPECHECK ERROR FOR BOTH, LHS&RHS
     else if ( (compareType table (getVal (subKids!!0)) (getVal (subKids!!1))) == False )
         then error  ("ERROR: TYPECHECK FAILED: "++(getVal (subKids!!0))++" and "++(getVal (subKids!!1))++" are of different types!")
-    --TYPECHECK ERROR FOR RHS: IF ID IN RHS ISN'T SAME TIME AS RHS ITSELF
+    --TYPECHECK ERROR FOR RHS: IF RHS IS A BOOLEANEXPR MULTIPLE [GOES HERE!!]
+    --else if ( ("Boolean Expression" `isInfixOf` (getVal (subKids!!1))) && (not (passTypeCheckBoolM table (subKids!!1))) )
+    --    then error "ERROR: TYPECHECK FAILED: [this error won't ever be reached, passScopeCheck used instead]*"
+    --TYPECHECK ERROR FOR RHS: IF ID IN RHS ISN'T SAME TYPE AS RHS ITSELF [just for IntExpr Multiples at the moment]
     else if (
                 (containsValidId (getVal (subKids!!1))) &&
                 (False `elem` (map (compareType table (getVal (subKids!!1))) (retrieveValidIds (getVal (subKids!!1)))))
@@ -558,14 +613,27 @@ processKids (kid@(Node "<Assign Statement>" subKids):kids) curScope hiScope scop
         processKids kids curScope hiScope scopeMap table
 
 --PrintStatement
---processKids (kid@(Node "<Print Statement>" subKids):kids) curScope hiScope scopeMap table
-
+processKids (kid@(Node "<Print Statement>" subKids):kids) curScope hiScope scopeMap table = 
+    --SCOPECHECK IF ID'S IN EXPR
+    if ( 
+            (containsValidId (getVal (subKids!!0))) && 
+            ( False `elem` (map (passScopeCheck curScope scopeMap table) (retrieveValidIds (getVal (subKids!!0)))) )
+       ) 
+        then error "ERROR: SCOPECHECK FAILED: [this error won't ever be reached. this is a passScopeCheck example.]"
+    --TYPECHECK ERROR FOR RHS: IF ID IN RHS ISN'T SAME TYPE AS RHS ITSELF [just for IntExpr Multiples at the moment]
+    else if (
+                (containsValidId (getVal (subKids!!0))) &&
+                (False `elem` (map (compareType table (getVal (subKids!!0))) (retrieveValidIds (getVal (subKids!!0)))))
+            )
+        then error ("ERROR: TYPECHECK FAILED: ID isn't of proper type!")
+    --PASSED SCOPECHECK AND TYPECHECK
+    else
+        processKids kids curScope hiScope scopeMap table
 
 
 --PATTERN NOT REACHED
 processKids _ _ _ _ _ = error "ERROR: Pattern not reached in processKids!!!"
-
-
+    
 
 
 
@@ -587,21 +655,6 @@ retrieveValidIds s = filter (isValidId) (map (unpack) (splitOn (pack ",") (pack 
 
 
 
-
---for and from dated code. didn't have the heart to get rid of it.
-removeAll :: String -> String -> String
-removeAll s toReturn = 
-    if (s `isInfixOf` toReturn) 
-        then removeAll s (toReturn\\s)
-    else 
-        toReturn
-
-    
-
-
-
-
-
 --creates a sample table
 testTable :: SymbolTable
 testTable = SymbolTable [ 
@@ -610,6 +663,10 @@ testTable = SymbolTable [
                             (IDRow "c" "string"     1),
                             (IDRow "d" "int"        2) 
                         ]
+
+emptyTable :: SymbolTable
+emptyTable = SymbolTable []
+
 
 testScope0 :: SCOPE
 testScope0 = (SCOPE 0 [(SCOPE 1 [(SCOPE 2 [])])])
@@ -621,6 +678,8 @@ testScope2 :: SCOPE
 testScope2 = (SCOPE 2 [])
 
 
+testTree :: Tree String
+testTree = Node "<Boolean Expression>" [(Node "5" []), (Node "==" []), (Node "true" [])]
 
 
 
@@ -629,8 +688,7 @@ testScope2 = (SCOPE 2 [])
 
 
 
-
-
+    
 
 
 

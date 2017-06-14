3a255e546b8ef3cd5b468f99a894c14e3e3668d5
Brian Steiner <briansteiner95@gmail.com>
2017-06-14 17:56:19 -0400

    Added an instance of Show for Memory. ZZ is "hex" for empty

--- CodeGen.hs
+++ CodeGen.hs
@@ -8,6 +8,8 @@ import Data.Char (intToDigit)
 import Data.Maybe
 
 
+testMemory :: Memory
+testMemory = (Memory [(Adr "12" "1"), (Adr "12" "ZZ")] (Reg "ZZ") (Reg "ZZ") (Reg "ZZ"))
 
 --Conversion helper functions
 hexToDec :: Hex -> Int
@@ -29,7 +31,7 @@ data Address = Adr {loc :: Loc, val :: Val} deriving (Eq)
 
 data Register = Reg Val deriving (Eq, Show)
 
-data Memory = Memory {addresses :: [Address], xReg :: Register, yReg :: Register, acc :: Register} deriving (Eq, Show)
+data Memory = Memory {addresses :: [Address], xReg :: Register, yReg :: Register, acc :: Register} deriving (Eq)
 
 instance Show Address where
     show adr@(Adr str@('$':xs) val) = str++":    "++val
@@ -38,8 +40,21 @@ instance Show Address where
 instance Ord Address where
     compare a@(Adr numA _) b@(Adr numB _) = compare (hexToDec (drop 1 numA)) (hexToDec (drop 1 numB))
 
+
+
+
+--Helper Functions
 getLoc :: Address -> Hex
 getLoc adr@(Adr str _) = (replicate (4 - (length str)) '0')++(loc adr)
+
+
+instance Show Memory where
+    show mem@(Memory adrs x@(Reg vx) y@(Reg vy) a@(Reg va)) =
+        "X:    "++vx++"\nY:    "++vy++"\nA:    "++va++"\n"++(concat (map ((++"\n").(show)) (filter ((/="ZZ").(val)) adrs)))
+
+
+
+--initializeAddresses :: 
 
 
 

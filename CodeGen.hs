82c483114da0d48baf4ab2a1e960563f1ad37a7f
Brian Steiner <briansteiner95@gmail.com>
2017-06-14 16:23:13 -0400

    made a decToHex function

--- CodeGen.hs
+++ CodeGen.hs
@@ -2,7 +2,9 @@ module CodeGen where
 
 import AST
 import Data.List
-import Data.Hex 
+import Data.Hex (hex, unhex)
+import Numeric (showHex, showIntAtBase)
+import Data.Char (intToDigit)
 import Data.Maybe
 
 
@@ -11,19 +13,23 @@ import Data.Maybe
 hexChar ch = fromMaybe (error $ "illegal char " ++ [ch]) $ 
     elemIndex ch "0123456789ABCDEF"
 
-parseHex hex = foldl' f 0 hex where
+hexToDec hex = foldl' f 0 hex where
     f n c = 16*n + hexChar c
 
-data Address = Adr String deriving (Eq)
+decToHex dec = showIntAtBase 16 intToDigit dec ""
+
+type Hex = String
+
+data Address = Adr Hex deriving (Eq)
 
 instance Show Address where
     show adr@(Adr str@('$':xs)) = str
     show adr@(Adr str) = '$':(replicate (4 - (length str)) '0')++str
 
 instance Ord Address where
-    compare a@(Adr numA) b@(Adr numB) = compare (parseHex (drop 1 numA)) (parseHex (drop 1 numB))
+    compare a@(Adr numA) b@(Adr numB) = compare (hexToDec (drop 1 numA)) (hexToDec (drop 1 numB))
 
---data Register =  deriving (Eq, Show)
+data Register = Reg Hex
 
 
 

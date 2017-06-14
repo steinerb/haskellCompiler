6b11b4ce529b9199e0f45f51b7308c6d8b3b38f5
Brian Steiner <briansteiner95@gmail.com>
2017-06-14 15:58:39 -0400

    made a function to get the Decimal value of a Hex value

--- CodeGen.hs
+++ CodeGen.hs
@@ -1,12 +1,18 @@
 module CodeGen where
 
 import AST
-import Data.Hex
 import Data.List
+import Data.Hex 
+import Data.Maybe
 
-test :: String
-test = "test"
 
+
+
+hexChar ch = fromMaybe (error $ "illegal char " ++ [ch]) $ 
+    elemIndex ch "0123456789ABCDEF"
+
+parseHex hex = foldl' f 0 hex where
+    f n c = 16*n + hexChar c
 
 data Address = Adr String deriving (Eq)
 
@@ -15,6 +21,13 @@ instance Show Address where
     show adr@(Adr str) = '$':(replicate (4 - (length str)) '0')++str
 
 instance Ord Address where
-    compare a@(Adr numA) b@(Adr numB) = compare (hex (drop 1 numA)) (hex (drop 1 numB))
+    compare a@(Adr numA) b@(Adr numB) = compare (parseHex (drop 1 numA)) (parseHex (drop 1 numB))
 
---data Register = Reg Int deriving (Eq, Show)
+--data Register =  deriving (Eq, Show)
+
+
+
+
+
+
+
